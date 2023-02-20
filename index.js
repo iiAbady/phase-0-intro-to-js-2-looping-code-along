c49ad865bf5b3e00371a03cb0e6a29547b0784d7
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-02-21 02:46:34 +0300

    Completed Assignment!

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+function writeCards(names, thisEvent) {
+const messages = [];
+for (let i = 0; i < names.length; i++) {
+	const message = `Thank you, ${names[i]}, for the wonderful ${thisEvent} gift!`
+	messages.push(message);
+}
+return messages;
+}
+
+
+function countDown(number) {
+	let i = number;
+	while (i >= 0) {
+		console.log(i);
+		i--;
+	}
+}
